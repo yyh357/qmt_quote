fe1ed1e0b36bce782ea04e02a5060026127318d9
wukan <wu-kan@163.com>
2025-02-22 00:06:57 +0800

    修改文件名

--- examples/run_minute.bat
+++ examples/run_minute.bat
@@ -1,5 +1,5 @@
 echo disable quick edit, but not working in Windows Terminal
 reg add "HKCU\Console" /v QuickEdit /t REG_DWORD /d 0 /f
 CALL d:\Users\Kan\miniconda3\Scripts\activate.bat d:\Users\Kan\miniconda3\envs\py312
-python subscribe_1m.py
+python subscribe_minute.py
 pause
