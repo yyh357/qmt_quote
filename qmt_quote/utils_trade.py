1b4f9b2a236451575a87bf83b467d7e3e4d877f1
wukan <wu-kan@163.com>
2025-02-26 00:51:35 +0800

    价格与数量函数调整

--- qmt_quote/utils_trade.py
+++ qmt_quote/utils_trade.py
@@ -1,57 +1,112 @@
 import math
 
+import numpy as np
 import pandas as pd
 from numba import njit
+from xtquant import xtconstant
 from xtquant.xtconstant import *  # noqa
 
 
 def to_dict(obj):
+    """将xtquant对象转换为字典，排除私有属性和以m_开头的属性"""
     return {attr: getattr(obj, attr) for attr in dir(obj) if not attr.startswith(("__", "m_"))}
 
 
-def objs_to_dataframe(obj):
-    return pd.DataFrame.from_records([to_dict(o) for o in obj])
+def objs_to_dataframe(objs):
+    """对象列表转换为DataFrame"""
+    return pd.DataFrame.from_records([to_dict(o) for o in objs])
 
 
-def price_plan(is_buy: bool, bid_1: float, ask_1: float, last_price: float = 0.0):
-    # https://github.com/openctp/openctp/blob/master/demo/ctpping/ThostFtdcUserApiDataType.h#L747
+@njit
+def adjust_price0(is_buy: bool, priority: int, offset: int,
+                  bid_1: float, ask_1: float,
+                  last_price: float = 0.0, pre_close: float = 0.0,
+                  tick: float = 0.01) -> float:
+    """激进或保守，偏移下单
+
+    Parameters
+    ----------
+    is_buy:bool
+    priority:int
+        0 现价
+        1 对价
+        -1 挂价。撤单的概率高
+    offset:int
+        偏移。正数激进，负数保守，0表示保持不变
+    bid_1:float
+        卖一价
+    ask_1:float
+        买一价
+    last_price:float
+        最新价
+    pre_close:float
+        昨收价
+    tick:float
+        最小变动单位
+
+    Notes
+    -----
+    买一价加一跳不等于卖一价
+
+    """
     if is_buy:
-        pass
+        maker, taker = bid_1, ask_1
     else:
-        pass
+        maker, taker = ask_1, bid_1
+
+    if priority > 0:
+        price = taker or maker or last_price or pre_close
+    elif priority < 0:
+        price = maker or taker or last_price or pre_close
+    else:
+        price = last_price or pre_close
+
+    if is_buy:
+        price += offset * tick
+    else:
+        price -= offset * tick
+    return price
 
 
 @njit
 def adjust_price1(is_buy: bool, price: float,
                   bid_1: float, ask_1: float,
                   last_price: float = 0.0, pre_close: float = 0.0,
-                  ten_units: float = 0.1) -> float:
-    """价格笼子。集合竞价时没有价格笼子，连续竞价时才有价格笼子。
+                  tick: float = 0.01) -> float:
+    """价格笼子。集合竞价时没有价格笼子，可直接报涨跌停价
 
     （一）买入申报价格不得高于买入基准价格的102%和买入基准价格以上十个申报价格最小变动单位的孰高值；
     （二）卖出申报价格不得低于卖出基准价格的98%和卖出基准价格以下十个申报价格最小变动单位的孰低值。
 
+    Parameters
+    ----------
+    is_buy:bool
+    price
+        申报价格
+    bid_1
+        买一价
+    ask_1
+        卖一价
+    last_price
+        最新价
+    pre_close
+        昨收价
+    tick
+        最小变动单位
+
+    Returns
+    -------
+    float
+        调整后的价格
+
     """
+    tick10 = tick * 10
     if is_buy:
-        if ask_1 == 0:
-            if bid_1 == 0:
-                base_price = pre_close if last_price == 0 else last_price
-            else:
-                base_price = bid_1
-        else:
-            base_price = ask_1
-
-        return min(max(base_price * 1.02, base_price + ten_units), price)
+        base = ask_1 or bid_1 or last_price or pre_close
+        return min(max(base * 1.02, base + tick10), price)
     else:
-        if bid_1 == 0:
-            if ask_1 == 0:
-                base_price = pre_close if last_price == 0 else last_price
-            else:
-                base_price = ask_1
-        else:
-            base_price = bid_1
-
-        return max(min(base_price * 0.98, base_price - ten_units), price)
+        base = bid_1 or ask_1 or last_price or pre_close
+        return max(min(base * 0.98, base - tick10), price)
 
 
 @njit
@@ -60,24 +115,67 @@ def adjust_price2(is_buy: bool, price: float,
                   ndigits: int = 100) -> float:
     """买卖价不能超过涨跌停价
 
+    Parameters
+    ----------
+    is_buy:bool
+    price
+        申报价格
+    limit_down
+        跌停价
+    limit_up
+        涨停价
+    ndigits
+        价格精度，100表示0.01股票, 1000表示0.001基金, 5表示0.2股指期货
+
+    Returns
+    -------
+    float
+        调整后的价格
+
     Notes
     -----
-    TODO 上市第一天的涨幅一定时停牌，这里要再设计
+    TODO 上市第一天的涨幅停牌，这里要再设计
 
     """
     if is_buy:
+        # 买入时价格向下靠拢
         price = math.floor(price * ndigits) / ndigits
         return max(min(limit_up, price), limit_down)
     else:
+        # 卖出时价格向上靠拢
         price = math.ceil(price * ndigits) / ndigits
         return min(max(limit_down, price), limit_up)
 
 
-@njit
+# @njit
 def adjust_quantity(is_buy: bool, is_kcb: bool, quantity: int,
                     can_use_volume: int, tolerance: int = 10) -> int:
     """
     根据股票交易规则调整股票买卖数量
+
+    Parameters
+    ----------
+    is_buy:bool
+        是否买入
+    is_kcb:bool
+        是否科创板
+    quantity:int
+        申报数量
+    can_use_volume:int
+        可用数量. 已经排除了挂单卖出时的冻结frozen_volume
+    tolerance:int
+        容忍度。遇到数量差异时，是否调整数量。
+
+    Returns
+    -------
+    int
+        调整后的数量
+
+    Notes
+    -----
+    科创板：单笔申报数量不小于200股，以1股为单位递增
+    非科创板：买入时，数量向下取整到 100 的整数倍；卖出时，数量向上取整到 100 的整数倍
+
     """
     if is_buy:
         if is_kcb:
@@ -86,26 +184,27 @@ def adjust_quantity(is_buy: bool, is_kcb: bool, quantity: int,
                 # 少于一定数量，不买了
                 quantity = 0
             else:
+                # 最低200股
                 quantity = max(quantity, 200)
         else:
             # 买入时，数量向下取整到 100 的整数倍
             quantity = (quantity + tolerance) // 100 * 100
     else:
-        if is_kcb:
-            if can_use_volume <= 200:
-                quantity = can_use_volume
-            else:
-                quantity = max(quantity, 200)
-                quantity = min(quantity, can_use_volume)
-        else:
-            if can_use_volume <= 100:
-                quantity = can_use_volume
-            else:
-                quantity = quantity // 100 * 100
-                quantity = min(quantity, can_use_volume)
-
-        if can_use_volume - quantity < tolerance:
+        # C  Q
+        if quantity + tolerance >= can_use_volume:
+            # 数量大于可用数量，全部卖出
             quantity = can_use_volume
+        elif is_kcb:
+            if quantity + tolerance >= 200:
+                quantity = max(quantity, 200)
+            else:
+                quantity = 0
+        else:
+            # 1 Q 2 C 3
+            if quantity + tolerance >= 100:
+                quantity = (quantity + tolerance) // 100 * 100
+            else:
+                quantity = 0
 
     return quantity
 
@@ -155,3 +254,16 @@ def cancel_orders(trader, account, orders: pd.DataFrame, direction: int = 0) -> 
     for i, row in orders.iterrows():
         orders.loc[i, 'seq'] = trader.cancel_order_stock_async(account, row.order_id)
     return orders
+
+
+def send_orders(trader, account, transactions: pd.DataFrame, priority: int, offset: int, is_auction: bool, strategy_name: str, order_remark: str) -> pd.DataFrame:
+    transactions['seq'] = 0
+    transactions['order_type'] = np.where(transactions['is_buy'], xtconstant.STOCK_BUY, xtconstant.STOCK_SELL)
+    transactions['price'] = transactions.apply(lambda x: adjust_price0(x.is_buy, priority, offset, x.bidPrice_1, x.askPrice_1, x.lastPrice, x.lastClose, tick=0.01), axis=1)
+    if not is_auction:
+        transactions['price'] = transactions.apply(lambda x: adjust_price1(x.is_buy, x.price, x.bidPrice_1, x.askPrice_1, x.lastPrice, x.lastClose, tick=0.01), axis=1)
+    transactions['price'] = transactions.apply(lambda x: adjust_price2(x.is_buy, x.price, x.DownStopPrice, x.UpStopPrice, ndigits=100), axis=1)
+    transactions['order_volume'] = transactions.apply(lambda x: adjust_quantity(x.is_buy, x.is_kcb, x.quantity, x.can_use_volume, tolerance=10), axis=1)  #
+    for i, v in transactions.iterrows():
+        transactions.loc[i, 'seq'] = trader.order_stock_async(account, v.stock_code, v.order_type, v.order_volume, xtconstant.FIX_PRICE, v.price, strategy_name, order_remark)
+    return transactions
---
+++ tests/quantity.py
@@ -0,0 +1,19 @@
+from qmt_quote.utils_trade import adjust_quantity
+
+# for is_buy in [True, False]:
+#     for is_kcb in [True, False]:
+#         print(f'{is_buy=}, {is_kcb=}', '=' * 60)
+#         for quantity in range(30, 300, 60):
+#             for can_use_volume in range(0, 300, 100):
+#                 for tolerance in range(0, 30, 10):
+#                     print(f'{quantity=}, {can_use_volume=}, {tolerance=}', '>', adjust_quantity(is_buy, is_kcb, quantity, can_use_volume, tolerance))
+
+print('-' * 60)
+
+is_buy = False
+is_kcb = True
+quantity = 270
+can_use_volume = 200
+tolerance = 20
+print(f'{is_buy=},{is_kcb=}, {quantity=}, {can_use_volume=}, {tolerance=}')
+print(adjust_quantity(is_buy, is_kcb, quantity, can_use_volume, tolerance))
