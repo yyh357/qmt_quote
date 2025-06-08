8e26800c4184e178ba197ad182c4ac49192ba0f3
yyh357 <275822746@qq.com>
2025-06-08 20:04:46 +0800

    config

--- examples/config.py
+++ examples/config.py
@@ -12,14 +12,14 @@ https://imdisk-toolkit.en.lo4d.com/download
 
 # =====历史行情======
 # TODO 历史数据目录，使用实盘QMT标准版，可以手工下载历史数据，然后在miniquant中读出,也可用第三方数据
-DATA_DIR = r"D:\e海方舟-量化交易版\datadir"
+DATA_DIR = r"D:\ProgramFiles\国金证券QMT交易端\datadir"
 
 # TODO: 历史数据文件
-HISTORY_STOCK_1d = r"F:\stock_1d.parquet"
-HISTORY_INDEX_1d = r"F:\index_1d.parquet"
-HISTORY_STOCK_1m = r"F:\stock_1m.parquet"
-HISTORY_INDEX_1m = r"F:\index_1m.parquet"
-HISTORY_STOCK_5m = r"F:\stock_5m.parquet"
+HISTORY_STOCK_1d = r"D:\qmt_data\stock_1d.parquet"
+HISTORY_INDEX_1d = r"D:\qmt_data\index_1d.parquet"
+HISTORY_STOCK_1m = r"D:\qmt_data\stock_1m.parquet"
+HISTORY_INDEX_1m = r"D:\qmt_data\index_1m.parquet"
+HISTORY_STOCK_5m = r"D:\qmt_data\stock_5m.parquet"
 
 # =====实时行情======
 # TODO：行数，一定每天接收最大数据量上再扩充一些，防止溢出。溢出时会直接崩溃退出
@@ -47,7 +47,7 @@ FILE_d5m = r"M:\d5m.npy"
 FILE_d1d = r"M:\d1d.npy"
 
 # 备份目录
-BACKUP_DIR = r"F:\backup"
+BACKUP_DIR = r"D:\backup"
 
 # =====交易设置======
 # 信号
@@ -55,6 +55,6 @@ FILE_s1t = r"M:\s1t.npy"  # 顺序记录插入的信号
 FILE_s1d = r"M:\s1d.npy"  # 日频信号，用与下单
 
 # TODO 交易目录，miniquant版
-USERDATA_DIR = r"D:\迅投极速交易终端 睿智融科版\userdata_mini"
-# TODO: 交易账号
-ACCOUNT = "2025727"
+USERDATA_DIR = r"D:\ProgramFiles\国金QMT交易端模拟\userdata_mini"
+# TODO: 交易账号    
+ACCOUNT = "55005362"
